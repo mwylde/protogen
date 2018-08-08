1f3b2d2d66b0e80bc9b617bcd7d1f4526dbfaa84
Micah Wylde <micah@micahw.com>
2018-08-08 16:51:25 -0700

    Core language parsing is done

--- src/lib.rs
+++ src/lib.rs
@@ -75,7 +75,7 @@ hci_command = {
 
     #[test]
     fn message_args() {
-        let text = " hci_command (type: u8, name: String) = { }!";
+        let text = " hci_command ($type: u8, $name: String) = { }!";
 
         assert_eq!(
             message(text.as_bytes()),
@@ -99,7 +99,7 @@ hci_command = {
 
     #[test]
     fn message_args_with_values() {
-        let text = " hci_command (type: u8 = 0xFF, name: String = \"hello world!\") = { }!";
+        let text = " hci_command ($type: u8 = 0xFF, $name: String = \"hello world!\") = { }!";
 
         assert_eq!(
             message(text.as_bytes()),
@@ -210,11 +210,223 @@ hci_command = {
     }
 
     #[test]
+    fn choose() {
+        let text = r#"
+hci_message ($name: u32) = {
+  @type: u8;
+  public message: choose {
+    HciCommand = hci_command(@type) |
+    HciData = hci_data(@type, $name)
+  };
+}!"#;
+
+        assert_eq!(
+            message(text.as_bytes()),
+            Ok((&[33][..], Message {
+                name: "hci_message".to_string(),
+                args: vec![
+                    Arg {
+                        name: "name".to_string(),
+                        data_type: DataType::Value("u32".to_string()),
+                        value: None,
+                    },
+                ],
+                fields: vec![
+                    Field {
+                        public: false,
+                        variable: true,
+                        name: "type".to_string(),
+                        data_type: DataType::Value("u8".to_string()),
+                        value: None,
+                    },
+                    Field {
+                        public: true,
+                        variable: false,
+                        name: "message".to_string(),
+                        data_type: DataType::Choose(vec![
+                            ChooseVariant {
+                                name: "HciCommand".to_string(),
+                                data_type: DataType::Message {
+                                    name: "hci_command".to_string(),
+                                    args: vec!["@type".to_string()],
+                                }
+                            },
+                            ChooseVariant {
+                                name: "HciData".to_string(),
+                                data_type: DataType::Message {
+                                    name: "hci_data".to_string(),
+                                    args: vec![
+                                        "@type".to_string(),
+                                        "$name".to_string()
+                                    ],
+                                }
+                            },
+                        ]),
+                        value: None,
+                    }
+                ] })));
+    }
+
+    #[test]
+    fn array() {
+        let text = r#"
+hci_command = {
+  @type: [u8; 12];
+  @length: u8;
+  public @data: [u8; @length];
+}!"#;
+
+        assert_eq!(
+            message(text.as_bytes()),
+            Ok((&[33][..], Message {
+                name: "hci_command".to_string(),
+                args: vec![],
+                fields: vec![
+                    Field {
+                        public: false,
+                        variable: true,
+                        name: "type".to_string(),
+                        data_type: DataType::Array {
+                            data_type: Box::new(DataType::Value("u8".to_string())),
+                            length: Expression::Value(Value::Number(12))
+                        },
+                        value: None,
+                    },
+                    Field {
+                        public: false,
+                        variable: true,
+                        name: "length".to_string(),
+                        data_type: DataType::Value("u8".to_string()),
+                        value: None,
+                    },
+                    Field {
+                        public: true,
+                        variable: true,
+                        name: "data".to_string(),
+                        data_type: DataType::Array {
+                            data_type: Box::new(DataType::Value("u8".to_string())),
+                            length: Expression::Variable("@length".to_string())
+                        },
+                        value: None,
+                    },
+
+                ] })));
+    }
+
+    #[test]
+    fn apply() {
+        let text = r#"
+hci_command = {
+  public command: apply @data choose {
+    Reset = reset(@ocf) |
+    SetEventFilter = set_event_filter(@ocf)
+  };
+}!"#;
+
+        assert_eq!(
+            message(text.as_bytes()),
+            Ok((&[33][..], Message {
+                name: "hci_command".to_string(),
+                args: vec![],
+                fields: vec![
+                    Field {
+                        public: true,
+                        variable: false,
+                        name: "command".to_string(),
+                        data_type: DataType::Apply {
+                            source: "@data".to_string(),
+                            data_type: Box::new(DataType::Choose(vec![
+                                ChooseVariant {
+                                    name: "Reset".to_string(),
+                                    data_type: DataType::Message {
+                                        name: "reset".to_string(),
+                                        args: vec!["@ocf".to_string()],
+                                    },
+                                },
+                                ChooseVariant {
+                                    name: "SetEventFilter".to_string(),
+                                    data_type: DataType::Message {
+                                        name: "set_event_filter".to_string(),
+                                        args: vec!["@ocf".to_string()],
+                                    },
+                                }
+                            ])),
+                        },
+                        value: None,
+                    },
+
+                ] })));
+    }
+
+    #[test]
+    fn message_type() {
+        let text = r#"
+inquiry_result = {
+  public condition: filter_condition();
+}!"#;
+
+        assert_eq!(
+            message(text.as_bytes()),
+            Ok((&[33][..], Message {
+                name: "inquiry_result".to_string(),
+                args: vec![],
+                fields: vec![
+                    Field {
+                        public: true,
+                        variable: false,
+                        name: "condition".to_string(),
+                        data_type: DataType::Message {
+                            name: "filter_condition".to_string(),
+                            args: vec![],
+                        },
+                        value: None,
+                    },
+
+                ] })));
+    }
+
+    #[test]
     fn string_parser() {
         assert_eq!(string("\"hello world\"".as_bytes()),
         Ok((&[][..], Value::String("hello world".to_string()))))
     }
+
+    #[test]
+    fn test_messages() {
+        let text = r#"
+        hci_command = { }
+        hci_message = {}
+        "#;
+
+        assert_eq!(
+            source_file(text.as_bytes()),
+            Ok((&[][..], vec![
+                Message {
+                    name: "hci_command".to_string(),
+                    args: vec![],
+                    fields: vec![]
+                },
+                Message {
+                    name: "hci_message".to_string(),
+                    args: vec![],
+                    fields: vec![]
+                },
+            ])));
+    }
+//
+//    #[test]
+//    fn test_file() {
+//        let source = include_bytes!("../examples/hci_message.pg");
+//        println!("{:?}", source_file(source));
+//    }
 }
+
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+pub enum Expression {
+    Value(Value),
+    Variable(String),
+}
+
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
 pub enum DataType {
@@ -223,8 +435,19 @@ pub enum DataType {
 //    String(Option<String>),
 //    ByteArray(Option<Vec<u8>>),
     Value(String),
-    Message(MessageCall),
-    Choose(Vec<MessageCall>),
+    Array {
+        data_type: Box<DataType>,
+        length: Expression,
+    },
+    Message {
+        name: String,
+        args: Vec<String>
+    },
+    Choose(Vec<ChooseVariant>),
+    Apply {
+        source: String,
+        data_type: Box<DataType>,
+    }
 }
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
@@ -244,9 +467,9 @@ pub struct Field {
 }
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
-pub struct MessageCall {
+pub struct ChooseVariant {
     name: String,
-    args: Vec<DataType>
+    data_type: DataType
 }
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
@@ -308,9 +531,87 @@ named!(number<Value>,
     map!(alt_complete!(hex_number | dec_number), Value::Number));
 
 named!(value<Value>,
+  alt!(string | number));
+
+named!(variable<String>,
+    do_parse!(
+        sigil: map_res!(alt!(tag!("@") | tag!("$")), str::from_utf8) >>
+        name: symbol >>
+        (
+             [sigil, name].join("").to_string()
+        )));
+
+named!(message_type<DataType>,
+    ws!(do_parse!(
+        name: symbol >>
+        args: delimited!(
+            tag!("("),
+            separated_list!(tag!(","), variable),
+            tag!(")")) >>
+        (
+           DataType::Message { name: name.to_string(), args }
+        ))));
+
+named!(choose_type<DataType>,
+    ws!(do_parse!(
+        _choose: tag!("choose") >>
+        variants: delimited!(
+            tag!("{"),
+            separated_list!(tag!("|"),
+                do_parse!(
+                    name: symbol >>
+                    _eq: tag!("=") >>
+                    data_type: data_type >>
+                    (
+                        ChooseVariant {
+                            name: name.to_string(),
+                            data_type
+                        }
+                    )
+                )
+            ),
+            tag!("}")
+        ) >>
+        (
+            DataType::Choose(variants)
+        ))));
+
+named!(apply_type<DataType>,
+    ws!(do_parse!(
+         _apply: tag!("apply") >>
+         source: variable >>
+         data_type: data_type >>
+         (
+             DataType::Apply {
+                 source,
+                 data_type: Box::new(data_type)
+             }
+         ))));
+
+named!(expression<Expression>,
+    ws!(alt!(
+       complete!(variable) => {|v| Expression::Variable(v)} |
+       complete!(value)    => {|v| Expression::Value(v)})));
+
+named!(array_type<DataType>,
+    ws!(delimited!(
+        tag!("["),
+        map!(separated_pair!(data_type, tag!(";"), expression),
+            |ab| DataType::Array { data_type: Box::new(ab.0), length: ab.1}),
+        tag!("]"))));
+
+named!(data_type<DataType>,
+    ws!(alt!(
+        complete!(array_type) |
+        complete!(choose_type) |
+        complete!(apply_type) |
+        complete!(message_type) |
+        map!(complete!(symbol), |s| DataType::Value(s.to_string())))));
+
+named!(assign_value<Value>,
     ws!(do_parse!(
         _equals: tag!("=") >>
-        value: alt!(string | number) >>
+        value: value >>
         ( value ))));
 
 named!(field<Field>,
@@ -319,15 +620,15 @@ named!(field<Field>,
         variable: opt!(tag!("@")) >>
         name: symbol >>
         _colon: tag!(":") >>
-        type_name: symbol >>
-        value: opt!(value) >>
+        data_type: data_type >>
+        value: opt!(assign_value) >>
         _semicolon: tag!(";") >>
         (
             Field {
                public: public.is_some(),
                variable: variable.is_some(),
                name: name.trim().to_string(),
-               data_type: DataType::Value(type_name.trim().to_string()),
+               data_type,
                value
             }
         ))));
@@ -337,10 +638,11 @@ named!(args<Vec<Arg>>,
         tag!("("),
         separated_list!(tag!(","),
             do_parse!(
+                _sigil: tag!("$") >>
                 name: symbol >>
                 _colon: tag!(":") >>
                 type_name: symbol >>
-                value: opt!(value) >>
+                value: opt!(assign_value) >>
                 (
                    Arg {
                       name: name.to_string(),
@@ -365,3 +667,12 @@ named!(pub message<Message>,
             fields,
         }
       ))));
+
+named!(pub source_file<Vec<Message>>,
+    ws!(many0!(complete!(message))));
+
+pub fn parse(data: &[u8]) -> Result<Vec<Message>, Nom::Error> {
+    // this is really hacky but seems like the easiest way to work around
+    // https://github.com/Geal/nom/issues/795 for now
+
+}
