3bef8723319ced2abaacb4a677285f03e2d90e39
Micah Wylde <micah@micahw.com>
2018-08-09 16:52:31 -0700

    Refactored code generator to include intermediate representation; working support for enum types

--- src/generator.rs
+++ src/generator.rs
@@ -1,10 +1,13 @@
 use parser::{DataType, Field, Message, source_file};
+use std::fmt;
+use std::collections::HashMap;
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use parser::Expression;
     use parser::Value;
+    use parser::ChooseVariant;
 
     #[test]
     fn test_to_camel_case() {
@@ -14,19 +17,22 @@ mod tests {
 
     #[test]
     fn test_data_type() {
-        assert_eq!("u8", render_data_type(&DataType::Value("u8".to_string())));
+        fn t(prefix: &str, dt: &DataType) -> String {
+            Generator::render_data_type(prefix, &mut vec![], dt)
+        }
 
-        assert_eq!("Vec<u8>", render_data_type(&DataType::Array {
+        assert_eq!("u8", t("", &DataType::Value("u8".to_string())));
+        assert_eq!("Vec<u8>", t("", &DataType::Array {
                 data_type: Box::new(DataType::Value("u8".to_string())),
                 length: Expression::Value(Value::Number(8)),
             }));
 
-        assert_eq!("HciCommand", render_data_type(&DataType::Message {
+        assert_eq!("HciCommand", t("", &DataType::Message {
             name: "hci_command".to_string(),
             args: vec![],
         }));
 
-        assert_eq!("Vec<u8>", render_data_type(&DataType::Apply {
+        assert_eq!("Vec<u8>", t("", &DataType::Apply {
             source: "data".to_string(),
             data_type: Box::new(DataType::Array {
                 data_type: Box::new(DataType::Value("u8".to_string())),
@@ -34,7 +40,7 @@ mod tests {
             })
         }));
 
-        assert_eq!("Vec<Vec<HciCommand>>", render_data_type(&DataType::Array {
+        assert_eq!("Vec<Vec<HciCommand>>", t("", &DataType::Array {
             length: Expression::Value(Value::Number(8)),
             data_type: Box::new(DataType::Array {
                 length: Expression::Value(Value::Number(8)),
@@ -45,28 +51,55 @@ mod tests {
             })
         }));
 
-//        assert_eq!("HciCommand_Command", render_data_type(&DataType::Choose(
-//            vec![]
-//        )));
-    }
+        let dt = DataType::Choose(vec![
+            ChooseVariant {
+                name: "HciCommand".to_string(),
+                data_type: DataType::Array {
+                    data_type: Box::new(DataType::Value("u8".to_string())),
+                    length: Expression::Value(Value::Number(8)),
+                },
+            },
+            ChooseVariant {
+                name: "HciData".to_string(),
+                data_type: DataType::Choose(vec![
+                    ChooseVariant {
+                        name: "Something".to_string(),
+                        data_type: DataType::Value("String".to_string()),
+                    }
+                ])
+            }
+        ]);
 
-    #[test]
-    fn test_field() {
-        assert_eq!(Some("    data: u8,".to_string()), render_struct_field(&Field {
-            public: true,
-            variable: false,
-            name: "data".to_string(),
-            data_type: DataType::Value("u8".to_string()),
-            value: None,
-        }));
+        let mut enums = vec![];
+        assert_eq!("HciMessage_Message", Generator::render_data_type(
+            "HciMessage_Message", &mut enums, &dt));
 
-        assert_eq!(None, render_struct_field(&Field {
-            public: true,
-            variable: false,
-            name: "data".to_string(),
-            data_type: DataType::Value("u8".to_string()),
-            value: Some(Value::Number(10)),
-        }));
+        let expected = vec![
+            Enum {
+                name: "HciMessage_Message_HciData".to_string(),
+                variants: vec![
+                    EnumVariant {
+                        name: "Something".to_string(),
+                        data_type: "String".to_string()
+                    }
+                ]
+            },
+            Enum {
+                name: "HciMessage_Message".to_string(),
+                variants: vec![
+                    EnumVariant {
+                        name: "HciCommand".to_string(),
+                        data_type: "Vec<u8>".to_string()
+                    },
+                    EnumVariant {
+                        name: "HciData".to_string(),
+                        data_type: "HciMessage_Message_HciData".to_string()
+                    }
+                ]
+            }
+        ];
+
+        assert_eq!(expected, enums);
     }
 
     #[test]
@@ -75,9 +108,15 @@ mod tests {
 pub struct HciCommand {
     ocf: u8,
     length: u8,
-}"#;
+    message: HciCommand_Message,
+}
 
-        assert_eq!(expected.trim(), render_struct(&Message {
+pub enum HciCommand_Message {
+    SomeMessage(Vec<u8>),
+}
+"#;
+
+        assert_eq!(expected.trim(), Generator::from_messages(vec![Message {
             name: "hci_command".to_string(),
             args: vec![],
             fields: vec![
@@ -101,8 +140,66 @@ pub struct HciCommand {
                     name: "data".to_string(),
                     data_type: DataType::Value("u8".to_string()),
                     value: Some(Value::Number(10)),
+                },
+                Field {
+                    public: false,
+                    variable: false,
+                    name: "message".to_string(),
+                    data_type: DataType::Choose(vec![
+                        ChooseVariant {
+                            name: "SomeMessage".to_string(),
+                            data_type: DataType::Array {
+                                data_type: Box::new(DataType::Value("u8".to_string())),
+                                length: Expression::Value(Value::Number(8)),
+                            },
+                        }]),
+                    value: None,
                 }
-            ] }));
+            ] }]).unwrap().to_string().trim());
+    }
+
+    #[test]
+    fn test_struct_field() {
+       assert_eq!("length: u8",
+                  format!("{}", StructField { name: "length".to_string(), data_type: "u8".to_string() }));
+    }
+
+    #[test]
+    fn test_struct() {
+        let s = Struct {
+            name: "HciCommand".to_string(),
+            fields: vec![
+                StructField { name: "length".to_string(), data_type: "u8".to_string() },
+                StructField { name: "name".to_string(), data_type: "String".to_string()},
+            ]
+        };
+
+        let expected = r#"
+pub struct HciCommand {
+    length: u8,
+    name: String,
+}"#;
+
+        assert_eq!(expected.trim(), format!("{}", s));
+    }
+
+    #[test]
+    fn test_enum() {
+        let e = Enum {
+            name: "SetEventFilter_Filter".to_string(),
+            variants: vec![
+                EnumVariant { name: "ClearAllFilter".to_string(), data_type: "ClearAllFilter".to_string()},
+                EnumVariant { name: "InquiryResult".to_string(), data_type: "String".to_string()},
+            ]
+        };
+
+        let expected = r#"
+pub enum SetEventFilter_Filter {
+    ClearAllFilter(ClearAllFilter),
+    InquiryResult(String),
+}"#;
+
+        assert_eq!(expected.trim(), format!("{}", e));
     }
 
     #[test]
@@ -110,10 +207,10 @@ pub struct HciCommand {
         let source: &str = include_str!("../examples/hci_message.pg");
         let messages = source_file(source.trim().as_bytes()).unwrap().1;
 
-        for m in messages {
-            println!("{}", render_struct(&m));
-        }
+        let generator = Generator::from_messages(messages).unwrap();
+        println!("{}", generator);
     }
+
 }
 
 //pub fn generate(message: Message) {
@@ -144,36 +241,154 @@ pub fn to_camel_case(s: &str, initial_cap: bool) -> String {
     result
 }
 
-fn render_data_type(data_type: &DataType) -> String {
-    match *data_type {
-        DataType::Value(ref s) => s.clone(),
-        DataType::Array { ref data_type, .. } => {
-            format!("Vec<{}>", render_data_type(&*data_type))
-        },
-        DataType::Message { ref name, .. } => to_camel_case(name, true),
-        DataType::Apply { ref data_type, .. } => {
-            render_data_type(&*data_type)
-        },
-        _ => "unsupported".to_string()
+
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+struct StructField {
+    name: String,
+    data_type: String,
+}
+
+impl fmt::Display for StructField {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}", self.name, self.data_type)
     }
 }
 
-fn render_struct_field(field: &Field) -> Option<String> {
-    if field.value.is_none() {
-        Some(format!("    {}: {},", field.name, render_data_type(&field.data_type)))
-    } else {
-        None
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+struct Struct {
+    name: String,
+    fields: Vec<StructField>
+}
+
+impl fmt::Display for Struct {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "pub struct {} {{\n", self.name)?;
+
+        for field in &self.fields {
+            write!(f, "    {},\n", field)?;
+        }
+
+        write!(f, "}}")
     }
 }
 
-fn render_struct(message: &Message) -> String {
-    let fields = message.fields.iter()
-        .map(render_struct_field)
-        .flat_map(|s| s)
-        .collect::<Vec<String>>()
-        .join("\n");
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+struct EnumVariant {
+    name: String,
+    data_type: String,
+}
 
-    format!("pub struct {} {{\n{}\n}}",
-            to_camel_case(&message.name, true),
-            fields)
+impl fmt::Display for EnumVariant {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}({})", self.name, self.data_type)
+    }
+}
+
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+struct Enum {
+    name: String,
+    variants: Vec<EnumVariant>
+}
+
+impl fmt::Display for Enum {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "pub enum {} {{\n", self.name)?;
+
+        for field in &self.variants {
+            write!(f, "    {},\n", field)?;
+        }
+
+        write!(f, "}}")
+    }
+}
+
+pub struct Generator {
+    messages: Vec<Message>,
+    structs: HashMap<String, Struct>,
+    enums: HashMap<String, Enum>,
+}
+
+impl Generator {
+    fn render_data_type(prefix: &str, enums: &mut Vec<Enum>, data_type: &DataType) -> String {
+        match data_type {
+            DataType::Value(ref s) => s.clone(),
+            DataType::Array { ref data_type, .. } => {
+                format!("Vec<{}>", Generator::render_data_type(prefix, enums, &*data_type))
+            },
+            DataType::Message { ref name, .. } => to_camel_case(name, true),
+            DataType::Apply { ref data_type, .. } => {
+                Generator::render_data_type(prefix, enums, &*data_type)
+            },
+            DataType::Choose(variants) => {
+                let e = Enum {
+                    name: prefix.to_string(),
+                    variants: variants.iter().map(|v| {
+                        EnumVariant {
+                            name: v.name.clone(),
+                            data_type: Generator::render_data_type(
+                                &[prefix, &to_camel_case(&v.name, true)].join("_"),
+                                enums,
+                                &v.data_type),
+                        }
+                    }).collect(),
+                };
+
+                enums.push(e);
+                prefix.to_string()
+            }
+        }
+    }
+
+    pub fn from_messages(messages: Vec<Message>) -> Result<Generator, String> {
+        let mut structs = HashMap::new();
+        let mut enums: Vec<Enum> = vec![];
+        for message in &messages {
+            let mut s = Struct {
+                name: to_camel_case(&message.name, true),
+                fields: vec![],
+            };
+
+            for f in &message.fields {
+                let prefix = &[&s.name[..], &to_camel_case(&f.name, true)].join("_");
+                let data_type = Generator::render_data_type(prefix, &mut enums, &f.data_type);
+
+                if f.value.is_none() {
+                    s.fields.push(StructField {
+                        name: f.name.clone(), data_type,
+                    });
+                }
+            }
+
+            if structs.contains_key(&s.name) {
+                return Err(format!("duplicate struct type {}", s.name));
+            }
+
+            structs.insert(s.name.clone(), s);
+        }
+
+        let mut enum_map = HashMap::new();
+        for e in enums {
+            if enum_map.contains_key(&e.name) {
+                return Err(format!("duplicate enum type {}", e.name));
+            }
+            enum_map.insert(e.name.clone(), e);
+        }
+
+        // TODO: validate that all referenced message types exist
+
+        Ok(Generator { messages, structs, enums: enum_map })
+    }
+}
+impl fmt::Display for Generator {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for s in self.structs.values() {
+            write!(f, "{}\n\n", s)?;
+        }
+
+        for e in self.enums.values() {
+            write!(f, "{}\n\n", e)?;
+        }
+
+        Ok(())
+    }
 }
